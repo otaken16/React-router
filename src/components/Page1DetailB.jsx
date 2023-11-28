bc404557745c9405acd93c7709a48a507c9f2ad2
ken.shin <ken16.tky@gmail.com>
2023-11-28 13:31:22 +0900

    ✨ 機能追加

--- src/components/Page1DetailB.jsx
+++ src/components/Page1DetailB.jsx
@@ -1,1 +1,10 @@
-export const Page1DetailB = () => <h1>Page1DetailBページです</h1>;
+import { useNavigate } from "react-router-dom";
+export const Page1DetailB = () => {
+  const navigate = useNavigate();
+  const onClickBack = () => navigate(-1);
+  return(
+    <>
+      <h1>Page1DetailBページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </>
+)};
